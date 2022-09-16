caa9e639b8f505d27e12e7c8ff7c854b9d7887a1
iamsahanasr <sahanasr24@gmail.com>
2022-09-17 00:19:32 +0530

    single line if else

---
+++ single_line_if_else.py
@@ -0,0 +1,19 @@
+num1=input("enter the first number\n")
+num2=input("enter the second number\n")
+
+msg="less" if num1<num2 else "greater or equal"
+print(msg)
+
+'''
+output:-
+enter the first number
+1
+enter the second number
+2
+less
+
+nter the first number
+4
+enter the second number
+2
+greater or eqaul
