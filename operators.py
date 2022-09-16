96266232aa33304eb89a596e433e9decd113a499
iamsahanasr <sahanasr24@gmail.com>
2022-09-17 00:07:42 +0530

    operators-bool-operations

---
+++ operators.py
@@ -0,0 +1,41 @@
+# a=10;b=20
+# print(f"Sum is {a+b}") #Sum is 20
+# print(f"Difference is {a-b}") #Difference is -10
+# print(f"Product is {a*b}") #Product is 200
+# print(f"Floating point quotient is {a/b}") #Floating point quotient is 0.5
+# print(f"Integer quotient is {a//b}") #integer quotient is 0
+# print(f"Remainder is {a%b}") #Remainder is 0
+# print(f"power is {a**2}") #power is 100
+
+# a="hello";b="world"
+# print(f"Sum is {a+b}") #helloworld
+# print(f"Difference is {a-b}") #error
+# print(f"Product is {a*b}") #error
+# print(f"Floating point quotient is {a/b}") #error
+# print(f"Integer quotient is {a//b}") #error
+# print(f"Remainder is {a%b}") #error
+# print(f"power is {a**2}") #error
+
+# c=2
+# print(f"Product is {a*c}") #hellohello
+
+d=True;e=True
+print(f"Sum is {d+e}") #Sum is 2
+print(f"Difference is {d-e}") #Difference is 0
+print(f"Product is {d*e}") #Product is 1
+print(f"Floating point quotient is {d/e}") #Floating point quotient is 1.0
+print(f"Integer quotient is {d//e}") #Integer quotient is 1
+print(f"Remainder is {d%e}") #Remainder is 0
+print(f"power is {d**e}") #Power is 1
+
+d=True;e=False
+print(f"Sum is {d+e}")  #Sum is 1
+print(f"Difference is {d-e}") #Difference is 1
+print(f"Product is {d*e}") #Product is 0
+print(f"Floating point quotient is {d/e}") #error
+print(f"Integer quotient is {d//e}") #error
+print(f"Remainder is {d%e}") #error
+print(f"power is {d**e}") #error
+
+
+
