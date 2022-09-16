73ac92d360caa7ec61c9f39dfdeaa3fda1a54caf
iamsahanasr <sahanasr24@gmail.com>
2022-09-17 00:12:18 +0530

    bitwise-operators

---
+++ bitwise_operators.py
@@ -0,0 +1,7 @@
+a,b=0b000,0b001
+print(f"a and b {a&b}") #output:-0
+print(f"a or b {a|b}")  #output:-1
+print(f"a xor b {a^b}") #output:-1
+print(b<<1) #left shift output:- 2
+print(b>>1) #right shift output:- 0
+print(~a)   #compliment
